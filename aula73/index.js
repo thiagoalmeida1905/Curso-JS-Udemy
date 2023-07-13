6b17d28f5483cee95eaca4a42523bf0b88495c6b
thiagoalmeida1905 <thi.almeida1905@gmail.com>
2023-07-13 20:40:47 -0300

    lesson 73 - useful methods for objects

---
+++ aula73/index.js
@@ -0,0 +1,19 @@
+/*
+Object.values
+Object.entries // permite converter um objeto em um array de pares chave/valor, o que facilita o acesso e manipulação dos dados contidos no objeto.
+Object.getOwnPropertyDescriptor(o, 'prop')
+Object.assign(des, any)
+... (spread)
+
+// Já vimos
+Object.keys (retorna as chaves)
+Object.freeze (congela o objeto)
+Object.defineProperties (define várias propriedades)
+Object.defineProperty (define uma propriedade)
+*/
+const produto = { nome: 'Produto', preco: 1.8, material: 'porcelana' };
+
+for(let valor of Object.entries(produto)) {
+  console.log(valor[0], valor[1]);
+}
+
