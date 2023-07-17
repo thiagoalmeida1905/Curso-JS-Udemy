5c75682c8af8592b3cf8626f5702121f35b2f732
thiagoalmeida1905 <thi.almeida1905@gmail.com>
2023-07-17 20:22:18 -0300

    lesson 79 -  map

---
+++ aula79/index.js
@@ -0,0 +1,17 @@
+const pessoas = [
+    { id:3, nome: 'Luiz'},
+    { id:2, nome: 'Luiz'},
+    { id:1, nome: 'Luiz'},
+];
+
+// const novasPessoas = {};
+// for (const pessoa of pessoas) {
+//     const {id} = pessoa;
+//     novasPessoas[id] = {...pessoa };
+// }
+
+const novasPessoas = new Map();
+for (const pessoa of pessoas) {
+    const {id} = pessoa;
+    novasPessoas.set(id, {...pessoa});
+}
