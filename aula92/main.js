03d0c7c2a6955afb9d1a8bdbd06071247b4d5f12
thiagoalmeida1905 <thi.almeida1905@gmail.com>
2023-07-24 11:52:12 -0300

    lesson 92 - babel

---
+++ aula92/main.js
@@ -0,0 +1,13 @@
+// const nome = 'Thiago';
+// const obj = { nome };
+// const novoObj = { ...obj };
+// console.log(novoObj);
+
+
+
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
