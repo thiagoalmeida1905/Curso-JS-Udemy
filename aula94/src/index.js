651d475193ff9eaf18f3ecb43aabddb3d34b07ce
thiagoalmeida1905 <thi.almeida1905@gmail.com>
2023-07-25 13:08:30 -0300

    lesson 94 - modules - import/export

--- aula94/src/index.js
+++ aula94/src/index.js
@@ -1,3 +1,5 @@
+//os módulos são uma forma de organizar e encapsular o código em arquivos separados, permitindo que diferentes partes de um programa sejam divididas em unidades lógicas e reutilizáveis. 
+
 import Pessoa, { nome, sobrenome } from './modulo1';
 
 const p1 = new Pessoa(nome, sobrenome);
