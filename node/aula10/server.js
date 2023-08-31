e93d90f1a7780dfe4ba9acc0e7a52c1ab5781ea0
thiagoalmeida1905 <thi.almeida1905@gmail.com>
2023-08-31 11:22:56 -0300

    lesson 10 - views (express)

--- node/aula10/server.js
+++ node/aula10/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const path = require('path');
+
 
 app.use(express.urlencoded({ extended: true }));
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs'); //para poder executar comandos que o html por si só não faz, p. ex: for, if etc...
+
 app.use(routes);
 
 app.listen(3000, () => {
