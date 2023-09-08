f1708c10c1b7eb79c88625132aab538b6d65b4d8
thiagoalmeida1905 <thi.almeida1905@gmail.com>
2023-09-08 18:48:54 -0300

    projetoagenda - read and Delete

--- projetoagenda/src/controllers/contatoController.js
+++ projetoagenda/src/controllers/contatoController.js
@@ -50,8 +50,21 @@ exports.edit = async function(req, res) {
         req.flash('success', 'Contato editado com sucesso.');
         req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
         return;
-        } catch(e) {
+    } catch(e) {
         console.log(e);
         res.render('404');
-        }
-    };
+    }
+};
+
+exports.delete = async function(req, res) {
+    if (!req.params.id) return res.render('404');
+
+    const contato = await Contato.delete(req.params.id);
+    if(!contato) return res.render('404');
+
+    req.flash('success', 'Contato apagado com sucesso.');
+    req.session.save(() => res.redirect('back'));
+    return;
+};
+
+//`/contato/index/${contato.contato._id}`
--- projetoagenda/src/models/contatoModel.js
+++ projetoagenda/src/models/contatoModel.js
@@ -17,12 +17,6 @@ function Contato(body) {
     this.contato = null;
 };
 
-Contato.buscaPorId = async function(id) {
-    if(typeof id !== 'string' ) return;
-    const contato = await ContatoModel.findById(id);
-    return contato;
-}
-
 Contato.prototype.register = async function() {
     this.valida();
     if(this.errors.length > 0) return;
@@ -59,6 +53,24 @@ Contato.prototype.edit = async function (id) {
     this.valida();
     if(this.errors.length > 0 ) return;
     this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true });
+};
+
+// Metodos estáticos
+Contato.buscaPorId = async function(id) {
+    if(typeof id !== 'string' ) return;
+    const contato = await ContatoModel.findById(id);
+    return contato;
+}
+Contato.buscaContatos = async function() {
+    const contatos = await ContatoModel.find()
+        .sort({criadoEm: -1 });
+    return contatos;
+}
+
+Contato.delete = async function(id) {
+    if(typeof id !== 'string') return;
+    const contato = await ContatoModel.findOneAndDelete({_id: id});
+    return contato;
 };
 
 module.exports = Contato;
